5320783d39c27b027c8ac6c6026cab3f128a799a
Mubashar Khokhar <m.khokhar@social-gizmo.com>
2025-07-08 16:56:35 +0200

    Laravel Workflow Database migration updated to use .env file

--- database/sql/mysql-init.sh
+++ database/sql/mysql-init.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# Load environment variables from the .env file
+if [ -f ".env" ]; then
+    echo "Loading environment variables from .env file..."
+    source .env
+else
+    echo ".env file not found!"
+    exit 1
+fi
+
 # Run your SQL script every time
 echo "Running db_export.sql..."
 
-mysql -h "$DB_HOST" -u "$DB_USER" -P "$DB_PORT" "$DB_DATABASE" < "${PWD}/database/sql/db_export.sql"
+mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" -P "$DB_PORT" "$DB_DATABASE" < "${PWD}/database/sql/db_export.sql"
 
 # Call original entrypoint command
 exec "$@"
