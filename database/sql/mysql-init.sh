50e7eb0e612ec3227e1eaf8a82a4a8a917e88281
Mubashar Khokhar <m.khokhar@social-gizmo.com>
2025-07-08 13:32:03 +0200

    Database import script updated
    Database import script updated
    
    Database import script updated
    
    Database import script updated

--- database/sql/mysql-init.sh
+++ database/sql/mysql-init.sh
@@ -3,7 +3,7 @@
 # Run your SQL script every time
 echo "Running db_export.sql..."
 
-mysql -h mysql -u root -ppassword laravel < /sql/db_export.sql
+mysql -h "$DB_HOST" -u "$DB_USER" -P "$DB_PORT" "$DB_DATABASE" < "${PWD}/database/sql/db_export.sql"
 
 # Call original entrypoint command
 exec "$@"
