8c6d35fa42ebe4a974ce3c356fa970a1ca3d07d3
Mubashar Khokhar <m.khokhar@social-gizmo.com>
2025-07-11 18:43:45 +0200

    DB import shell script updated

--- database/sql/mysql-init.sh
+++ database/sql/mysql-init.sh
@@ -30,5 +30,27 @@ echo "Running db_export.sql : import data to database ${DB_DATABASE}..."
 
 mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" -P "$DB_PORT" "$DB_DATABASE" < "${PWD}/sql/db_export.sql"
 
+#!/bin/bash
+
+DB_CHECK=$(mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" -P "$DB_PORT" -D "$DB_DATABASE" -N -s -e "
+    SELECT table_name
+    FROM information_schema.tables
+    WHERE table_schema = '$DB_DATABASE'
+    AND table_name IN ('api_apikey', 'transaktion_transaktionen', 'stamd_flagbit_ref', 'vorgaben_zeitraum');
+")
+
+# Convert output to array
+readarray -t tables <<< "$DB_CHECK"
+
+# Check for each table
+for t in "api_apikey" "transaktion_transaktionen" "vorgaben_zeitraum" "stamd_flagbit_ref"; do
+    if [[ " ${tables[*]} " == *" $t "* ]]; then
+        echo "Table '$t' exists."
+    else
+        echo "Table '$t' does not exist."
+    fi
+done
+
+
 # Call original entrypoint command
 exec "$@"
